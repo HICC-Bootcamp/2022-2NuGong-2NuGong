9454db67245a9f4cdc162cd7ddc7377a83a5bd1f
uwoobeat <desktop3711@g.hongik.ac.kr>
2022-07-23 15:46:38 +0900

    feat: Implement models.py

--- backend/rest_api/models.py
+++ backend/rest_api/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    # 일반 유저 생성
+    def create_user(self, nickname, department, favorites, password=None):
+        if not nickname:
+            raise ValueError("username required")
+        if not department:
+            raise ValueError("department required")
+        if not favorites:
+            raise ValueError("favorites required")
+        
+        user = self.model(
+            nickname = nickname,
+            department = department,
+            favorites = favorites,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    # 관리자 유저 생성
+    def create_admin_user(self, nickname, department, favorites, password=None):
+        user = self.create_user(
+            nickname = nickname,
+            password = password,
+            department = department,
+            favorites = favorites
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    nickname = models.CharField(max_length=10)
+    department = models.IntegerField()
+    favorites = ArrayField(models.IntegerField(), blank=True)
+
+    # User 모델의 필수 field
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    # 헬퍼 클래스 사용
+    objects = UserManager()
+
+    # username field는 nickname으로 설정
+    USERNAME_FIELD = 'nickname'
+    # 필수 작성 field
+    REQUIRED_FIELDS = ['department', 'favorites']
+
+    def __str__(self):
+        return self.name
+
+
+class Notice(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=30)
+    create_at = models.DateTimeField()
+    views = models.PositiveIntegerField()
+    tag = models.IntegerField()
+    department = models.IntegerField()
+    contents = models.TextField()
+
+    def __str___(self):
+        return self.title
+
+
+class Bookmark(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE, db_column="user_id")
+    notice_id = models.ForeignKey("Notice", on_delete=models.CASCADE, db_column="notice_id")
+
+    def __str__(self):
+        return self.id
