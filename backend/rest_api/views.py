cc29b81c46a1d651825c7916fca2bb9631a6929d
Cho Sangwook <chosw1002@naver.com>
2022-07-25 10:51:25 +0900

    FEAT: Add login and create function to views.py

--- backend/rest_api/views.py
+++ backend/rest_api/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.auth import AuthToken
+from .serializer import RegisterSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def login(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True) #username과 password가 옳지 않을 때 raise exception
+    user = serializer.validated_data['user']
+
+    _, token = AuthToken.objects.create(user)
+
+    return Response({
+        'user_info': {
+            'id': user.id,
+            'username': user.username
+        },
+        "token": token
+    })
+
+
+@api_view(['POST'])
+def createAccount(request):
+    serializer = RegisterSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+
+    _, token = AuthToken.objects.create(user)
+
+
+    return Response({
+        'user_info': {
+            'id': user.id,
+            'username': user.username
+        },
+        "token": token
+    })
+
+
