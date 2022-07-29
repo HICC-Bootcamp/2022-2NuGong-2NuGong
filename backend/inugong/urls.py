663f7d772719d819c424758ea2ca480b6467a99f
uwoobeat <desktop3711@g.hongik.ac.kr>
2022-07-30 07:03:36 +0900

    fix: Update urls for NoticeSuggestionListAPI

--- backend/inugong/urls.py
+++ backend/inugong/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_api.views import NoticeListAPI, NoticeDetailAPI, SearchAPI
+from rest_api.views import NoticeListAPI, NoticeDetailAPI, SearchAPI, NoticeSuggestionListAPI
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('rest_api.urls')),
     path('notice/', NoticeListAPI.as_view()),
+    path('notice/recommend/', NoticeSuggestionListAPI.as_view()),
     path('notice/<int:pk>/', NoticeDetailAPI.as_view()),
     path('search/', SearchAPI.as_view()),
 ]
