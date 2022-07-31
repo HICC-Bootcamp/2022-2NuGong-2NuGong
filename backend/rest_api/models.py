908057643eab9ff3ffabfe21647d795e4151100d
uwoobeat <desktop3711@g.hongik.ac.kr>
2022-07-31 19:39:15 +0900

    fix: Update model fields

--- backend/rest_api/models.py
+++ backend/rest_api/models.py
@@ -10,7 +10,11 @@ class UserManager(BaseUserManager):
         if not nickname:
             raise ValueError("nickname required")
         #아래줄의 리스트에서 0번째 인덱스는 tag번호와 인덱스를 일치시키기 위헤 존재함. 즉, 사용하지 않음.
-        user = self.model(nickname = nickname, favorites = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], subscribe = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        user = self.model(
+            nickname = nickname, 
+            favorites = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+            subscribe = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        )
 
         user.set_password(password)
         user.save(using=self._db)
@@ -29,9 +33,10 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
 class Notice(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=50)
     create_at = models.DateTimeField()
     views = models.PositiveIntegerField()
     tag = models.IntegerField()
@@ -42,9 +47,10 @@ class Notice(models.Model):
     def __str___(self):
         return self.title
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
-    nickname = models.CharField(max_length=10, unique=True)
+    nickname = models.CharField(max_length=20, unique=True)
     department = models.IntegerField(null=True)
     favorites = ArrayField(models.IntegerField(), null=True) #태그 별 조회수 저장, 추후 컬럼명 favorites -> tagviews으로 변경 필요
     subscribe = ArrayField(models.IntegerField(), null=True) #관심 태그 저장
@@ -65,5 +71,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.nickname
-
-
